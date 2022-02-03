2fd7aad1720507ab0a7cb5cfc74b06c5b9bb490e
miguel.lobo <miguel.lobo@feedzai.com>
2022-02-03 17:41:25 +0000

    PULSEDEV-36588 openml-api: Add ExplainableClassificationModel interface
    So that we have an interface for models that support explanations

---
+++ openml-api/src/main/java/com/feedzai/openml/data/ExplanationsConfig.java
@@ -0,0 +1,9 @@
+package com.feedzai.openml.data;
+
+import java.io.Serializable;
+
+/**
+ * Carries algorithm specific parameters required for computation of feature contributions.
+ */
+public abstract class ExplanationsConfig implements Serializable {
+}
